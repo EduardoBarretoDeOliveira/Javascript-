a7ef66ea1ef93a1063f202e7fb1288f4f9346c80
EduardoBarretoDeOliveira <100542424+EduardoBarretoDeOliveira@users.noreply.github.com>
2022-05-02 15:19:38 -0300

    ex09...012

---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}km/h`);
+if(vel > 70){
+    console.log(`Multado`);
+}
+console.log(`Dirija sempre usando sempre cinto de segurança`)
---
+++ aula11/ex010.js
@@ -0,0 +1,7 @@
+pais = 'Estados Unidos Da América'
+if (pais == 'Brasil'){
+    console.log(`salve cria`);
+} else{
+    console.log(`Vasa gringo otário`);
+    console.log(`Voce nasceu em ${pais} n respeito quem vem de la`);
+}
