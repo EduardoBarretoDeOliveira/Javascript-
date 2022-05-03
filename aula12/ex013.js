c5cfd5fcf6c69d211ff0a93cc444e7049efddf3e
EduardoBarretoDeOliveira <100542424+EduardoBarretoDeOliveira@users.noreply.github.com>
2022-05-03 15:13:20 -0300

    ex13ao15

---
+++ aula12/ex013.js
@@ -0,0 +1,9 @@
+idade = 16
+if (idade <16){
+    console.log(`Menó`);
+}else if (idade <18 || idade >65){
+    console.log(`idade de botar o butico pra jogo `);
+}else{
+    console.log(`Di maió`);
+}
+    
