8db51998afab9af627ec5bde982f95348f7666b5
EduardoBarretoDeOliveira <100542424+EduardoBarretoDeOliveira@users.noreply.github.com>
2022-05-09 16:53:45 -0300

    ex01

--- exerciciop1/ex01/texugo.js
+++ exerciciop1/ex01/texugo.js
@@ -1,13 +1,18 @@
-data = new Date()
-hora = data.getHours()
-min = data.getMinutes()
-horas = window.document.getElementById(`hora`)
-horas.innerHTML = (`Agora são ${hora}:${min}`)
-fundo = window.document.body.background-color
-if(hora < 12){
-    fundo.innerHTML = 
-} else if(hora <19){
-
-} else{
-    
+function load(){    
+    data = new Date()
+    var imga = window.document.getElementById(`imagem`)
+    hora = data.getHours()
+    min = data.getMinutes()
+    horas = window.document.getElementById(`hora`)
+    horas.innerHTML = (`Agora são ${hora}:${min}`)
+    if (hora > 5 && hora <13) {
+        imga.src = 'midias/dia.jpg'
+        document.body.style.background = '#f9de77'
+    } else if (hora < 19 && hora >13) {
+        imga.src = 'midias/tarde.jpg'
+        document.body.style.background = '#d85106'
+    } else {
+        imga.src = 'midias/noite.jpg'
+        document.body.style.background = '#4e3f5c'
+    }
 }
