cfa6b5e37ff43a4f634d9ef1ed808e78c5d2072d
EduardoBarretoDeOliveira <100542424+EduardoBarretoDeOliveira@users.noreply.github.com>
2022-05-21 19:04:04 -0300

    verificador

--- exerciciop1/ex02/codigo.js
+++ exerciciop1/ex02/codigo.js
@@ -1,13 +1,40 @@
 function ver(){
-    data = new Date()
-    anod = data.getFullYear()
-    anop = document.getElementById(`ano`)
-    res = document.getElementById(`res`)
+    var data = new Date()
+    var anod = data.getFullYear()
+    var anop = document.getElementById(`ano`)
+    var res = document.getElementById(`res`)
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
     if (anop.value.length == 0){
         window.alert(`verifique os dados`)
     } else{
-        sexp = document.getElementsByName(`radsex`)
+        var sexp = document.getElementsByName(`radsex`)
         var idade = anod - Number(anop.value)
-        res. innerHTML = `idade calculada: ${idade}`
+        var genero = ''
+        if (sexp[0].checked){
+            genero = 'Homem'
+            if(idade< 11){
+                img.setAttribute('src', 'midias/bebe-homem.jpg')
+            }else if(idade < 19){
+                img.setAttribute('src', 'midias/jovem-homem.jpg')
+            }else if(idade <60){
+                img.setAttribute('src', 'midias/homem-maduro.jpg')
+            }else{
+                img.setAttribute('src', 'midias/homem-idoso.jpg')
+            }
+        } else{
+            genero = 'Mulher'
+            if(idade< 11){
+                img.setAttribute('src', 'midias/bebe-mulher.jpg')
+            }else if(idade < 25){
+                img.setAttribute('src', 'midias/jovem-mulher.jpg')
+            }else if(idade <60){
+                img.setAttribute('src', 'midias/mulher-madura.jpg')
+            }else{
+                img.setAttribute('src', 'midias/mulher-idosa.jpg')
+            }
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos <br/>`
+        res.appendChild(img)
     }
 }
