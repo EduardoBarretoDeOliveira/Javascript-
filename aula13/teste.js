6cb90c549296789e8cc5228d944f945656458ba3
EduardoBarretoDeOliveira <100542424+EduardoBarretoDeOliveira@users.noreply.github.com>
2022-05-23 15:19:21 -0300

    repet

---
+++ aula13/teste.js
@@ -0,0 +1,10 @@
+/*var c = 1
+while ( c <=10){
+    console.log(`sua irmã vai me mamar ${c} vezes`)
+    c++
+}*/
+var c = 1
+ do{
+     console.log(`A sua irmã vai me mamar ${c} vezes`)
+     c++
+ } while(c<5)
